69c54005fc99f2442944176f4137ebbc00e0d5a0
cavayman <cavayman@gmail.com>
2016-03-29 22:04:05 +0300

    first my  try

---
+++ TestProject/src/main/java/com/logos/study/Main.java
@@ -0,0 +1,32 @@
+package com.logos.study;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	Class.forName("com.mysql.jdbc.Driver");
+	
+	String user="root";
+	String password="123789";
+	String url="jdbc:mysql://localhost:3306/taxi";
+	
+	Connection conn=DriverManager.getConnection(url, user, password);
+	Statement statement= conn.createStatement();
+	String q1="select * from taxi.avto";
+	ResultSet s=statement.executeQuery(q1);
+	while(s.next())
+	{
+		System.out.println(s.getString(1)+"   "+s.getString(2)+"   "+s.getString(3)+"   "+s.getString(4));
+	}
+	
+	conn.close();
+	
+	
+	
+	
+}
+}
